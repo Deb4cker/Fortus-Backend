548bf91d633150d6d929fe5c52d2026fe73c7fda
Deb4cker <ndebacher@gmail.com>
2024-09-18 19:33:32 -0300

    New generic constants

--- src/main/java/com/pmnato/fortus/commons/constants/EntityRoutes.java
+++ src/main/java/com/pmnato/fortus/commons/constants/EntityRoutes.java
@@ -1,14 +1,11 @@
 package com.pmnato.fortus.commons.constants;
 
-import static com.pmnato.fortus.commons.constants.EntityName.*;
+import static com.pmnato.fortus.commons.constants.EntityName.USER;
 
 public final class EntityRoutes {
     public static final String SLASH = "/";
 
     public static final String USER_ROUTE = SLASH + USER;
-    public static final String EXERCISE_ROUTE = SLASH + EXERCISE;
-    public static final String EQUIPMENT_ROUTE = SLASH + EQUIPMENT;
-    public static final String TRAINING_ROUTE = SLASH + TRAINING;
 
     private EntityRoutes() {}
 }
--- src/main/java/com/pmnato/fortus/commons/constants/RestRoutes.java
+++ src/main/java/com/pmnato/fortus/commons/constants/RestRoutes.java
@@ -2,15 +2,11 @@ package com.pmnato.fortus.commons.constants;
 
 public final class RestRoutes {
 
-    public static final String BASE_ROUTE = "/";
-
-    public static final String CREATE = BASE_ROUTE + "create";
-    public static final String ALL    = BASE_ROUTE + "all";
-    public static final String ID     = BASE_ROUTE + "{id}";
-    public static final String EDIT   = BASE_ROUTE + "edit/{id}";
-    public static final String DELETE = BASE_ROUTE + "delete/{id}";
-
-    public static final String LOGIN  = BASE_ROUTE + "login";
+    public static final String ALL = "/all";
+    public static final String ID = "/{id}";
+    public static final String EDIT = "/edit/{id}";
+    public static final String DELETE = "/delete/{id}";
+    public static final String LOGIN = "/login";
 
     private RestRoutes() {}
 }
