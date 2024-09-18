05f4f52e12cd95b942e206569d8a40f7f4b9cc92
Deb4cker <ndebacher@gmail.com>
2024-09-18 19:33:32 -0300

    New generic constants

---
+++ src/main/java/com/pmnato/fortus/commons/constants/EntityRoutes.java
@@ -0,0 +1,11 @@
+package com.pmnato.fortus.commons.constants;
+
+import static com.pmnato.fortus.commons.constants.EntityName.USER;
+
+public final class EntityRoutes {
+    public static final String SLASH = "/";
+
+    public static final String USER_ROUTE = SLASH + USER;
+
+    private EntityRoutes() {}
+}
