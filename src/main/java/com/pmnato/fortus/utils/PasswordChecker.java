2a688a2e658c85eb32fa14d62b6b4dba7d3eaef0
Deb4cker <ndebacher@gmail.com>
2024-09-18 14:35:25 -0300

    Added Log4j to project; Logger test

--- src/main/java/com/pmnato/fortus/utils/PasswordChecker.java
+++ src/main/java/com/pmnato/fortus/utils/PasswordChecker.java
@@ -3,9 +3,10 @@ package com.pmnato.fortus.utils;
 import com.pmnato.fortus.entity.User;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
-public class PasswordChecker {
-    public boolean isCorrectPassword(User user, String passwordInput) {
+public final class PasswordChecker {
+    private PasswordChecker() {}
+
+    public static boolean isCorrectPassword(User user, String passwordInput) {
         String comparativePassword = Encryptor.sha256Of(passwordInput, user.getSalt());
         return user.getPassword().equals(comparativePassword);
     }
