606dd42ecc1381c43d2ce0c379bab91a7f51b00e
Deb4cker <ndebacher@gmail.com>
2024-09-25 15:27:59 -0300

    Base route constant

--- src/main/java/com/pmnato/fortus/commons/constants/RestRoutes.java
+++ src/main/java/com/pmnato/fortus/commons/constants/RestRoutes.java
@@ -2,12 +2,15 @@ package com.pmnato.fortus.commons.constants;
 
 public final class RestRoutes {
 
-    public static final String ALL = "/all";
-    public static final String CREATE = "/create";
-    public static final String ID = "/{id}";
-    public static final String EDIT = "/edit/{id}";
-    public static final String DELETE = "/delete/{id}";
-    public static final String LOGIN = "/login";
+    public static final String BASE_ROUTE = "/";
+
+    public static final String CREATE = BASE_ROUTE + "create";
+    public static final String ALL    = BASE_ROUTE + "all";
+    public static final String ID     = BASE_ROUTE + "{id}";
+    public static final String EDIT   = BASE_ROUTE + "edit/{id}";
+    public static final String DELETE = BASE_ROUTE + "delete/{id}";
+
+    public static final String LOGIN  = BASE_ROUTE + "login";
 
     private RestRoutes() {}
 }
