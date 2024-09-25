d355fca9ed4b87331eaed75da4bdff6c24568140
Deb4cker <ndebacher@gmail.com>
2024-09-25 17:57:48 -0300

    Capitalized user in exception message

--- src/main/java/com/pmnato/fortus/exception/not_found/UserNotFoundException.java
+++ src/main/java/com/pmnato/fortus/exception/not_found/UserNotFoundException.java
@@ -1,7 +1,9 @@
 package com.pmnato.fortus.exception.not_found;
 
+import static com.pmnato.fortus.commons.constants.EntityName.CAPITALIZED_USER;
+
 public class UserNotFoundException extends NotFoundException{
     public UserNotFoundException() {
-        super("User not found!");
+        super(CAPITALIZED_USER);
     }
 }
