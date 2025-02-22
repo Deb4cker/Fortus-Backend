057357cc842cba68f3f62ab649a22ba3db840f48
Nicolas <ndebacher@gmail.com>
2025-02-22 02:01:28 -0300

    Preparing entity data load

--- src/main/java/com/pmnato/fortus/controller/EquipmentController.java
+++ src/main/java/com/pmnato/fortus/controller/EquipmentController.java
@@ -1,9 +1,7 @@
 package com.pmnato.fortus.controller;
 
 import com.pmnato.fortus.dto.EquipmentDto;
-import com.pmnato.fortus.service.EquipmentService;
 import com.pmnato.fortus.service.request.EquipmentRequest;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,41 +12,38 @@ import static com.pmnato.fortus.commons.constants.EntityRoutes.EQUIPMENT_ROUTE;
 import static com.pmnato.fortus.commons.constants.RestRoutes.*;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping(EQUIPMENT_ROUTE)
 public class EquipmentController extends BaseController<EquipmentDto, EquipmentRequest> {
-
-    private final EquipmentService equipmentService;
 
     @Override
     @GetMapping(ALL)
     public ResponseEntity<List<EquipmentDto>> getAll(){
-        return new ResponseEntity<>(equipmentService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
     @Override
     @PostMapping(CREATE)
     public ResponseEntity<Long> create(@RequestBody EquipmentRequest request) {
-        Long id = equipmentService.save(request);
+        Long id = service.save(request);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
     @Override
     @GetMapping(ID)
     public ResponseEntity<EquipmentDto> getById(@PathVariable long id) {
-        return new ResponseEntity<>(equipmentService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
     }
     @Override
     @PutMapping(EDIT)
     public ResponseEntity<Void> edit(@PathVariable Long id, @RequestBody EquipmentRequest request) {
-        equipmentService.update(id, request);
+        service.update(id, request);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
     @DeleteMapping(DELETE)
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        equipmentService.delete(id);
+        service.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/pmnato/fortus/entity/Exercise.java
+++ src/main/java/com/pmnato/fortus/entity/Exercise.java
@@ -1,26 +1,35 @@
 package com.pmnato.fortus.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.pmnato.fortus._enum.Difficulty;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static com.pmnato.fortus.commons.constants.JoinColumns.TRAINING_ID;
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
-
     private Difficulty difficultyLevel;
-
     private int repetitions;
-
     private String suggestedTime;
-
     private String imageUrl;
-
     private String VideoUrl;
 
+    @ManyToOne
+    @JoinColumn(name = TRAINING_ID, nullable = false)
+    @JsonBackReference
+    private Training training;
+
+    public Exercise(Long id, String name, Difficulty difficulty, int repetitions, String s, String s1, String s2) {
+    }
 }
