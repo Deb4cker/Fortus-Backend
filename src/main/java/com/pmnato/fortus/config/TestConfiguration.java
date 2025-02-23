d45e2471d9783ca3296935061ebb8b4902c3d201
Nicolas <ndebacher@gmail.com>
2025-02-23 17:13:54 -0300

    Preparing fake data creation

--- src/main/java/com/pmnato/fortus/config/TestConfiguration.java
+++ src/main/java/com/pmnato/fortus/config/TestConfiguration.java
@@ -3,6 +3,10 @@ package com.pmnato.fortus.config;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.pmnato.fortus.entity.Category;
+import com.pmnato.fortus.entity.Equipment;
+import com.pmnato.fortus.entity.Training;
+import com.pmnato.fortus.repository.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
@@ -11,7 +15,6 @@ import org.springframework.context.annotation.Profile;
 
 import static com.pmnato.fortus.commons.constants.Profiles.TEST_PROFILE;
 import com.pmnato.fortus.entity.User;
-import com.pmnato.fortus.repository.UserRepository;
 import com.pmnato.fortus.utils.Encryptor;
 
 import lombok.AllArgsConstructor;
@@ -21,12 +24,23 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TestConfiguration implements CommandLineRunner
 {
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final TrainingRepository trainingRepository;
+    private final ExerciseRepository exerciseRepository;
+    private final CategoryRepository categoryRepository;
+    private final EquipmentRepository equipmentRepository;
+    private final TrainingExerciseRepository trainingExerciseRepository;
+
     private static final Logger logger = LogManager.getLogger(TestConfiguration.class);
 
     @Override
     public void run(String... args) {
+        generateFakeCategories();
+        generateFakeExercises();
+        generateFakeEquipments();
         generateFakeUsers();
+        generateFakeTrainings();
+        generateFakeTrainingExercises();
     }
 
     private void generateFakeUsers(){
@@ -54,7 +68,23 @@ public class TestConfiguration implements CommandLineRunner
         }
 
         logger.info("Saving users...");
-        repository.saveAll(Arrays.asList(testUsers));
+        userRepository.saveAll(Arrays.asList(testUsers));
         logger.info("Fake users saved successfully.");
+    }
+
+    private void generateFakeTrainings(){
+        logger.info("Generating fake trainings...");
+    }
+    private void generateFakeCategories(){
+        logger.info("Generating fake categories...");
+    }
+    private void generateFakeEquipments(){
+        logger.info("Generating fake equipments...");
+    }
+    private void generateFakeExercises(){
+        logger.info("Generating fake exercises...");
+    }
+    private void generateFakeTrainingExercises(){
+        logger.info("Generating fake training exercises...");
     }
 }
---
+++ src/main/java/com/pmnato/fortus/repository/TrainingExerciseRepository.java
@@ -0,0 +1,7 @@
+package com.pmnato.fortus.repository;
+
+import com.pmnato.fortus.entity.TrainingExercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainingExerciseRepository extends JpaRepository<Long, TrainingExercise> {
+}
